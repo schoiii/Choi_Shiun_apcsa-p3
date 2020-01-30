83065cb4e146e2a3fcfbd50290ca62bbe5126a88
schoiii <hi1213141516171819@gmail.com>
2020-01-30 12:32:34 -0800

    Update 0a1.java

--- unit0Assignments/src/0a1.java
+++ unit0Assignments/src/0a1.java
@@ -10,7 +10,7 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Shiun Choi \t 1-30-19 \n\n" );
 		System.out.println("+++++++++++++++++++++++++ " );
 		System.out.println("+++++++++++++++++++++++++ " );
 		System.out.println("+++++++++++++++++++++++++ " );
