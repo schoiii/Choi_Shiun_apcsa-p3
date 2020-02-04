b390f4c041fa3ecba41c5e2302e7b70405fc151a
schoiii <hi1213141516171819@gmail.com>
2020-02-04 13:21:19 -0800

    oif;ogi;wrogi;weogihw

---
+++ unit4Assignments/src/DiscountRunner.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+		Discount test = new Discount();
+		System.out.println("Bill after discount :: "+test.getDiscountedBill(amt));
+
+	}
+}
