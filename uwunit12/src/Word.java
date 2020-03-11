8ff2e9fe420aa80f987a9ae8125b4760a7dea306
schoiii <hi1213141516171819@gmail.com>
2020-03-11 13:45:16 -0700

    asdff
    asdf

--- uwunit12/src/Word.java
+++ uwunit12/src/Word.java
@@ -10,15 +10,33 @@ public class Word implements Comparable<Word>
 
 	public Word( String s )
 	{
+		word=s;
 	}
-
+	public String get() {
+		return word;
+	}
+	
 	public int compareTo( Word rhs )
 	{		
+		
+		if(word.length()>rhs.get().length()) {
+			return 1;
+		}
+		if(word.length()<rhs.get().length())
+			return -1;
+		for(int i=0; i<Math.min(word.length(),rhs.get().length()); i++) {
+		if(word.charAt(i)>rhs.get().charAt(i)) {
+			return 1;
+		}
+		if(word.charAt(i)<rhs.get().charAt(i))
+			return -1;
+		}
 		return 0;
 	}
 
+	
 	public String toString()
 	{
-		return "";
+		return ""+word;
 	}
 }
--- uwunit12/src/WordRunner.java
+++ uwunit12/src/WordRunner.java
@@ -12,21 +12,16 @@ public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("words.dat"));
-
+		Scanner file = new Scanner(new File("C:\\Users\\chois2238\\Documents\\GitHub\\Choi_Shiun_apcsa-p3\\uwunit12\\src\\words.dat"));
 		int size = file.nextInt();
-		file.nextLine();
+		Word[] words=new Word[size];
+		for(int i=0; i<size; i++)
+			words[i]=new Word(file.nextLine());
 		
-
-
-
-
-
-
-
-
-
-
-
+		Arrays.sort(words);
+		
+		for(Word w : words) {
+			System.out.println(w);
+		}
 	}
 }
